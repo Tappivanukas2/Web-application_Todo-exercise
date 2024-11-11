cff5f42e1830cacd98eba858dfc961c8e3c7f241
Tatu Lehtola <tatu.lehtola1996@gmail.com>
2024-11-11 18:54:40 +0200

    5 osio tehty

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-import { pool } from './helpers/db.js'
+import todoRouter from './routers/todoRouter.js'
+//import { pool } from './routers/db.js'
 
 const port = process.env.PORT
 
@@ -8,39 +9,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use('/',todoRouter)
 
-app.get('/',(req,res) => {
-    pool.query('select * from task', (error, result) => {
-        if (error) {
-            return res.status(500).json({error: error.message})
-        }
-        return res.status(200).json(result.rows)
-    })
-})
-
-app.post('/create',(req,res) => {
-    pool.query('insert into task (description) values ($1) returning *',
-        [req.body.description],
-        (error,result) => {
-            if (error) {
-                return res.status(500).json({error: error.message})
-            }
-            return res.status(200).json({id: result.rows})
-        }
-    )
-})
-
-app.delete('/delete/:id',(req,res) => {
-    const id = parseInt(req.params.id)
-    pool.query('delete from task where id = $1',
-        [id],
-        (error,result) => {
-            if (error) {
-                return res.status(500).json({error: error.message})
-            }
-            return res.status(200).json({id: id})
-        }
-    )
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({error: err.message})
 })
 
 app.listen(port)
---
+++ server/routers/todoRouter.js
@@ -0,0 +1,43 @@
+import { pool } from "./db.js"
+import { Router } from "express"
+import { emptyOrRows } from "../helpers/utils.js"
+
+const router = Router()
+
+router.get('/',(req,res,next) => {
+    pool.query('select * from task', (error, result) => {
+        if (error) {
+            return next(error)
+            //return res.status(500).json({error: error.message})
+        }
+        return res.status(200).json(emptyOrRows(result))
+    })
+})
+
+router.post('/create',(req,res,next) => {
+    pool.query('insert into task (description) values ($1) returning *',
+        [req.body.description],
+        (error,result) => {
+            if (error) {
+                return next(error)
+                //return res.status(500).json({error: error.message})
+            }
+            return res.status(200).json({id: result.rows})
+        }
+    )
+})
+
+router.delete('/delete/:id',(req,res) => {
+    const id = parseInt(req.params.id)
+    pool.query('delete from task where id = $1',
+        [id],
+        (error,result) => {
+            if (error) {
+                return res.status(500).json({error: error.message})
+            }
+            return res.status(200).json({id: id})
+        }
+    )
+})
+
+export default router;
