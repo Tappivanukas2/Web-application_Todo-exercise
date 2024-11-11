497af4b3841f974d422e9dd791d1bc131467b538
Tatu Lehtola <tatu.lehtola1996@gmail.com>
2024-11-11 17:51:23 +0200

    Tunkkausta

--- server/index.js
+++ server/index.js
@@ -1,24 +1,15 @@
 import express from 'express'
 import cors from 'cors'
-import pkg from 'pg'
-import dotenv from 'dotenv'
+import { pool } from './helpers/db.js'
 
-const environment = process.env.NODE_ENV
-
-dotenv.config()
-
-const port = 3001
+const port = process.env.PORT
 
 const app = express()
-const { Pool } = pkg
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/',(req,res) => {
-
-    const pool = openDb()
-
     pool.query('select * from task', (error, result) => {
         if (error) {
             return res.status(500).json({error: error.message})
@@ -28,9 +19,6 @@ app.get('/',(req,res) => {
 })
 
 app.post('/create',(req,res) => {
-
-    const pool = openDb()
-
     pool.query('insert into task (description) values ($1) returning *',
         [req.body.description],
         (error,result) => {
@@ -43,7 +31,6 @@ app.post('/create',(req,res) => {
 })
 
 app.delete('/delete/:id',(req,res) => {
-    const pool = openDb()
     const id = parseInt(req.params.id)
     pool.query('delete from task where id = $1',
         [id],
@@ -55,16 +42,5 @@ app.delete('/delete/:id',(req,res) => {
         }
     )
 })
-
-const openDb = () => {
-    const pool = new Pool ({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: environment === 'development' ? process.env.DB_NAME : process.env.TEST_DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT
-    })
-    return pool
-}
 
 app.listen(port)
